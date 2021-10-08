053f951528d8d7973d8337fa68431133cfce2ba2
David Szamek <83954907+DavidSzamek@users.noreply.github.com>
2021-10-08 18:17:03 +1100

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -72,11 +72,11 @@ self.addEventListener("fetch", function(evt) {
       return;
     }
 
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-          return cache.match(evt.request).then(response => {
-              return response || fetch(evt.request);
-          });
-      })
-  );
+      evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
 });

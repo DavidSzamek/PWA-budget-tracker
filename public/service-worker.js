1471ed2299b42bdef86a452c153ac397a8b70d9d
David Szamek <83954907+DavidSzamek@users.noreply.github.com>
2021-10-08 19:02:39 +1100

    updated links in html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,17 @@
 // Files to Cache
 
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/index.js",
-    "db.js",
-    "/manifest.webmanifest",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
+  "/",
+  "/index.html",
+  "/db.js",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/styles.css",
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+
 ];
 
 const CACHE_NAME = "static-cache-v2";
